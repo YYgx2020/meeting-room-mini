897af903f387cd4107f9a1a8863ff8e6dd6a09ab
YYgx2020 <481585869@qq.com>
2023-05-18 19:33:16 +0800

    部署版本(配置了部署后的后端接口)

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,5 @@
 const IP = '10.33.88.150';
 // const IP = '127.0.0.1';
-const baseURL = `http://${IP}:4001`;
+// const baseURL = `http://${IP}:4001`;
+const baseURL = 'https://api.meeting.lianghongyi.com';
 export default baseURL;
